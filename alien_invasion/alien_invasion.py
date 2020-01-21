6b82050b1e70d5a59f374ec66e12c5af2cf4750b
hunzig <hunzigg@gmail.com>
2020-01-21 10:36:46 +0800

    20200121

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
 
+
 def run_game():
 	#初始化pygame、设置和屏幕对象
 	pygame.init()
@@ -13,10 +14,12 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 	
-	#创建一艘飞船
+	#创建一艘飞船、一个子弹编组和一个外星人编组
 	ship = Ship(ai_settings, screen)
-	#创建一个用于存储子弹的编组
 	bullets = Group()
+	aliens = Group()
+	#创建外星人群
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 	
 	#开始游戏主循环
 	while True:
@@ -24,10 +27,11 @@ def run_game():
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
 		bullets.update()
-		gf.update_bullets(bullets)
+		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+		gf.update_aliens(ai_settings, aliens)
 		
 		
 		#每次循环时都重绘屏幕 让最近绘制的屏幕可见
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 		
 run_game()
