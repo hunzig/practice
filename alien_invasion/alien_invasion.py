778d8873349598a5d0eaf7064b621c92cc106d6d
hunzig <hunzigg@gmail.com>
2020-02-07 13:53:06 +0800

    20200207

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
@@ -14,6 +15,9 @@ def run_game():
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 	
+	# 创建一个用于存储游戏统计信息的实例
+	stats = GameStats(ai_settings)
+	
 	#创建一艘飞船、一个子弹编组和一个外星人编组
 	ship = Ship(ai_settings, screen)
 	bullets = Group()
@@ -25,10 +29,12 @@ def run_game():
 	while True:
 		#监视键盘和鼠标事件
 		gf.check_events(ai_settings, screen, ship, bullets)
-		ship.update()
-		bullets.update()
-		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-		gf.update_aliens(ai_settings, aliens)
+		if stats.game_active:
+			ship.update()
+			bullets.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
 		
 		
 		#每次循环时都重绘屏幕 让最近绘制的屏幕可见
